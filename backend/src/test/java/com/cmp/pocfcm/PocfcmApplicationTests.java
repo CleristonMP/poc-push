dde4d050e57c37d8c81cccffcd835b9893aba06c
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-06-08 10:50:07 -0300

    Backend project created

---
+++ backend/src/test/java/com/cmp/pocfcm/PocfcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmp.pocfcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocfcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
